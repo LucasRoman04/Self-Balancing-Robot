6a60239e7487a9accfac94e84ae58681c5d4315a
Veniot <w0404614@campus.nscc.ca>
2024-04-23 14:12:07 -0300

    gyro angle calculated, onto pid next

--- Self-Balancing-Robot/Self-Balancing-Robot.ino
+++ Self-Balancing-Robot/Self-Balancing-Robot.ino
@@ -7,10 +7,13 @@
 #include "math.h"
  
 #define MIN_ABS_SPEED 20
+
+#define sampleTime 0.005
+#define baseAngle 1.22
  
 //motor / sensor objects
-AF_DCMotor motor1(3, MOTOR12_64KHZ);
-AF_DCMotor motor2(4, MOTOR12_64KHZ);
+AF_DCMotor motor1(3);
+AF_DCMotor motor2(4);
 Adafruit_MPU6050 mpu;
  
  
@@ -46,9 +49,13 @@ void loop() {
 
   int accelY, accelZ, gyroX, gyroRate, motorPower;
 
-  float accelAngle, gyroAngle = 0;
+  float accelAngle, gyroAngle = 0, prevAngle = 0;
+
+  volatile float currentAngle;
 
   unsigned long currTime, prevTime = 0, loopTime;
+
+
 
   mpu.getEvent(&a, &g, &temp);
  
@@ -63,17 +70,36 @@ accelY = a.acceleration.y;
 //use accel readings to figure out angle of inclination
 accelAngle = atan2(accelY, accelZ) * RAD_TO_DEG;
 
+gyroX = g.gyro.x;
+
+gyroRate = map(gyroX, -32768, 32767, 0, 255);
+
+// gyroAngle = gyroAngle + (float)gyroRate * loopTime/1000;
+
+// Serial.print("GyroAngle: ");
+Serial.println(gyroAngle);
+
+gyroAngle = (float) gyroRate * sampleTime;
+
+currentAngle = 0.9934 * (prevAngle + gyroAngle) + 0.0066 * (accelAngle);
+
+prevAngle = currentAngle;
+
+Serial.print("current angle: ");
+Serial.println(currentAngle);
+
 //basic movement logic
  if(isnan(accelAngle));
   else
-    motor1.setSpeed(0);
-    motor2.setSpeed(0);
-
-    if(accelAngle > 90){
+     motor1.setSpeed(115);
+     motor2.setSpeed(115);
+    // Serial.print("Accel Angle: ");
+    // Serial.println(accelAngle);
+    if(currentAngle > (baseAngle + 0.005)){
       motor1.run(BACKWARD);
       motor2.run(BACKWARD);
-    }
-    else if(accelAngle < 90){
+    } 
+    else if(currentAngle < (baseAngle - 0.005)){
       motor1.run(FORWARD);
       motor2.run(FORWARD);
     }
@@ -82,18 +108,7 @@ accelAngle = atan2(accelY, accelZ) * RAD_TO_DEG;
       motor2.run(RELEASE);
     }
  
-gyroX = g.gyro.x;
 
-gyroRate = map(gyroX, -32768, 32767, -250, 250);
 
-gyroAngle = gyroAngle + (float)gyroRate * loopTime/1000;
-
-Serial.println(gyroAngle);
- 
-//do something with readings (TO-DO)
-  float middlePoint = 0.16;
-  float difference = gyroX - middlePoint;
-
- 
 
 }
