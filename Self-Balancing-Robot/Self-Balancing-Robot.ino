538160b7aeb85ab9d42a3447b1d9e9ce35644012
Veniot <w0404614@campus.nscc.ca>
2024-04-24 11:01:49 -0300

    found set point

--- Self-Balancing-Robot/Self-Balancing-Robot.ino
+++ Self-Balancing-Robot/Self-Balancing-Robot.ino
@@ -1,114 +1,65 @@
-
-#include <PID_v1_bc.h>
+#include <Wire.h>
 #include <AFMotor.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
-#include "math.h"
- 
-#define MIN_ABS_SPEED 20
 
-#define sampleTime 0.005
-#define baseAngle 1.22
+#define SET_POINT 80
+#define MAX_SPEED 0
  
-//motor / sensor objects
-AF_DCMotor motor1(3);
-AF_DCMotor motor2(4);
 Adafruit_MPU6050 mpu;
- 
+AF_DCMotor motor1(4);
+AF_DCMotor motor2(3);
  
 void setup() {
-  //start serial clock
-  Serial.begin(9600);
-  Serial.println("Motor test!");
-  //set max speed for motors
-  Serial.println("Mpu 6050 test!");
+  Serial.begin(9600);        
 
-  //if mpu fails to start, prompt
   if(!mpu.begin()){
     Serial.println("Failed to find MPU 6050 chip");
     while(1){
       delay(10);
-    }
+     }
   }
-
-  Serial.println("MPU6050 found");
  
-  //set hardware modes
+  Serial.println("MPU6050 found");
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-
 }
  
 void loop() {
 
-  //set up event variables and collect a reading
-
   sensors_event_t a, g, temp;
-
-  int accelY, accelZ, gyroX, gyroRate, motorPower;
-
-  float accelAngle, gyroAngle = 0, prevAngle = 0;
-
-  volatile float currentAngle;
-
-  unsigned long currTime, prevTime = 0, loopTime;
-
-
-
   mpu.getEvent(&a, &g, &temp);
- 
- 
+  /* Print out the values */
+  // Serial.print("Acceleration x: ");
+  // Serial.println(a.acceleration.x, 5);
+  float accelAngle = atan2(a.acceleration.y, a.acceleration.z) * RAD_TO_DEG;
+  float difference = accelAngle - SET_POINT;
+  difference = angleError < 0 ? -angleError : angleError;
    
-currTime = millis();
-loopTime = currTime - prevTime;
-prevTime = currTime;
+  Serial.print("Angle: ");
+  Serial.println(accelAngle, 5);
+  // Serial.print("Error: ");
+  // Serial.println(angleError, 5);
 
-accelZ = a.acceleration.z;
-accelY = a.acceleration.y;
-//use accel readings to figure out angle of inclination
-accelAngle = atan2(accelY, accelZ) * RAD_TO_DEG;
 
-gyroX = g.gyro.x;
+  motor1.setSpeed(MAX_SPEED);
+  motor2.setSpeed(MAX_SPEED);
 
-gyroRate = map(gyroX, -32768, 32767, 0, 255);
-
-// gyroAngle = gyroAngle + (float)gyroRate * loopTime/1000;
-
-// Serial.print("GyroAngle: ");
-Serial.println(gyroAngle);
-
-gyroAngle = (float) gyroRate * sampleTime;
-
-currentAngle = 0.9934 * (prevAngle + gyroAngle) + 0.0066 * (accelAngle);
-
-prevAngle = currentAngle;
-
-Serial.print("current angle: ");
-Serial.println(currentAngle);
-
-//basic movement logic
- if(isnan(accelAngle));
-  else
-     motor1.setSpeed(115);
-     motor2.setSpeed(115);
-    // Serial.print("Accel Angle: ");
-    // Serial.println(accelAngle);
-    if(currentAngle > (baseAngle + 0.005)){
-      motor1.run(BACKWARD);
-      motor2.run(BACKWARD);
-    } 
-    else if(currentAngle < (baseAngle - 0.005)){
-      motor1.run(FORWARD);
-      motor2.run(FORWARD);
-    }
-    else{
-      motor1.run(RELEASE);
-      motor2.run(RELEASE);
-    }
- 
-
+  if(accelAngle ){
+    //falling backward
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+  }
+  else if (a.acceleration.x < .30) {
+    //falling forward
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+  }
+  else{
+    //balanced
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);    
+  }
 
 
 }
