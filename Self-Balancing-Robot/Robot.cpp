5c33294e41515ff16e8682d366638d27df2fd6cd
Veniot <w0404614@campus.nscc.ca>
2024-04-29 13:29:11 -0300

    Added PID Values

--- Self-Balancing-Robot/Robot.cpp
+++ Self-Balancing-Robot/Robot.cpp
@@ -1,51 +1,70 @@
+#include "Arduino.h"
+#include "PID_v1.h"
 #include "Robot.h"
 
 void Robot::run() {
 
-
+  // pidController = pidController(&this->input, &this->output, &this->SET_POINT, this->)
   mpu.getEvent(&this->a, &this->g, &this->temp);
 
-  float accelAngle = atan2(this->a.acceleration.y, this->a.acceleration.z) * RAD_TO_DEG;
-  float difference = accelAngle - this->SET_POINT;
-  difference = difference < 0 ? -difference : difference;
+  //grab time
+  this->currentTime = millis();
+  this->elapsedTime = currentTime - previousTime;
 
-  // Serial.print("Angle: ");
-  // Serial.println(accelAngle, 5);
-  // Serial.print("Difference: ");
-  // Serial.println(difference, 5);
+  //only calculate error if enough time has passed
+  if (elapsedTime >= SAMPLE_TIME) {
+    previousTime = currentTime;  //save time for next iteration.
 
-  // Motor speed using PROPORTIONAL
-  float motorSpeed = (this->PROPORTIONAL * difference);
+    float accelAngle = atan2(this->a.acceleration.y, this->a.acceleration.z) * RAD_TO_DEG;
+    this->difference = accelAngle - this->SET_POINT;
+    this->difference = this->difference < 0 ? -this->difference : this->difference;
 
-  // Motor speed using map
-  // float motorSpeed = map(difference, MIN_DIFFERENCE, MAX_DIFFERENCE, 100, 255);
 
-  motorSpeed = constrain(motorSpeed, MIN_SPEED, MAX_SPEED);
-  this->motor1.setSpeed(motorSpeed);
-  this->motor2.setSpeed(motorSpeed);
+    Serial.print("Angle: ");
+    Serial.println(accelAngle, 5);
+    // Serial.print("Difference: ");
+    // Serial.println(difference, 5);
 
-  if (accelAngle > this->SET_POINT && difference > this->FORWARD_MARGIN) {
+    // Motor speed using PROPORTIONAL
+    this->proportional = (this->KP * this->difference);
+    this->integral += KI * this->difference;
+    this->derivative = KD * (this->difference - this->lastDifference);
 
-    // motorSpeed += 45;
-    motorSpeed += map(difference, 0, 20, 0, 135);
-    motorSpeed = constrain(motorSpeed, this->MIN_SPEED, this->MAX_SPEED);
+
+    //calculate motorSpeed with pid
+    this->motorSpeed = proportional + integral + derivative;
+    this->lastDifference = this->difference;
+
+    // Motor speed using map
+    //     float motorSpeed = map(difference, MIN_DIFFERENCE, MAX_DIFFERENCE, 100, 255);
+
+    this->motorSpeed = constrain(motorSpeed, MIN_SPEED, MAX_SPEED);
     this->motor1.setSpeed(motorSpeed);
     this->motor2.setSpeed(motorSpeed);
 
-    //falling backward
-    this->motor1.run(BACKWARD);
-    this->motor2.run(BACKWARD);
+    if (accelAngle > this->SET_POINT && difference > this->FORWARD_MARGIN) {
 
-  } else if (accelAngle < this->SET_POINT && difference > this->BACK_MARGIN) {
+      // motorSpeed += 45;
+      // motorSpeed += map(difference, 0, 20, 0, 135);
+      motorSpeed = constrain(motorSpeed, this->MIN_SPEED, this->MAX_SPEED);
+      // this->motor1.setSpeed(motorSpeed);
+      // this->motor2.setSpeed(motorSpeed);
 
-    this->motor1.run(FORWARD);
-    this->motor2.run(FORWARD);
-  } else {
-    this->motor1.run(RELEASE);
-    this->motor2.run(RELEASE);
+      //falling backward
+      this->motor1.run(BACKWARD);
+      this->motor2.run(BACKWARD);
+
+    } else if (accelAngle < this->SET_POINT && difference > this->BACK_MARGIN) {
+
+      this->motor1.run(FORWARD);
+      this->motor2.run(FORWARD);
+    } else {
+      this->motor1.run(RELEASE);
+      this->motor2.run(RELEASE);
+    }
+    // Serial.print("Speed: ");
+    // Serial.println(motorSpeed);
   }
-  Serial.print("Speed: ");
-  Serial.println(motorSpeed);
 }
 
 Robot::Robot()
@@ -65,4 +84,6 @@ void Robot::init() {
   //set mpu modes
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+  //set up pid controller
 }
--- Self-Balancing-Robot/Robot.h
+++ Self-Balancing-Robot/Robot.h
@@ -4,28 +4,41 @@
 #include "Arduino.h"
 #include "AFMotor.h"
 #include <Wire.h>
-#include <AFMotor.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-
+#include <AFMotor.h>
+#include <PID_v1.h>
 
 class Robot {
 
 private:
-  const float SET_POINT = 81.6;
+  const float SET_POINT = 85;
   const float MIN_SPEED = 103;
   const float MAX_SPEED = 255;
-  const float PROPORTIONAL = 19;
-  const float FORWARD_MARGIN = .2;
-  const float BACK_MARGIN = .2;
+
+  const float FORWARD_MARGIN = 0.001;
+  const float BACK_MARGIN = 0.001;
+  const float SAMPLE_TIME = 0.005; //Time in ms between readings
+  const float KP = 25;
+  const float KI = 200;
+  const float KD = 0;
+
+  float motorSpeed;
+  float proportional;
+  float integral = 0;
+  float derivative = 0;
+
+  float difference;
+  float lastDifference = 0;
+
+  long currentTime = 0;
+  long previousTime = 0;
+  long elapsedTime = 0;
 
   Adafruit_MPU6050 mpu;
   AF_DCMotor motor1;
   AF_DCMotor motor2;
   sensors_event_t a, g, temp;
-
-  void get_reading();
-  void movement();
 
 public:
   Robot();
