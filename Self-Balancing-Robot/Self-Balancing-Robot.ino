71712273682cf85662e0ea6911174b82944fc16d
Veniot <w0404614@campus.nscc.ca>
2024-05-02 14:28:05 -0300

    Added watchdog, fixed major jittering issues, figured out min speed, adjusting P

--- Self-Balancing-Robot/Self-Balancing-Robot.ino
+++ Self-Balancing-Robot/Self-Balancing-Robot.ino
@@ -1,11 +1,10 @@
-#include <ArduPID.h>
-
 #include <AFMotor.h>
 #include "Arduino.h"
 #include "AFMotor.h"
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <avr/wdt.h>
 
 Adafruit_MPU6050 mpu;
 AF_DCMotor motor1(3);
@@ -13,27 +12,21 @@ AF_DCMotor motor2(4);
 
 sensors_event_t a, g, temp;
 
-double error       = 0;
-double errorSum    = 0;
-double currentTime = 0;
-double elapsedTime = 0;
-double prevTime    = 0;
-double frontMargin = 1.5;
-double backMargin  = 1.5;
+double error = 0;
+double frontMargin = 0.75;
+double backMargin = 0.75;
 
-
-double setpoint = 87.75;
+double setpoint = 86.35;
 double angle;
 double motorSpeed = 0;
-double integral = 0;
-double minSpeed = 103;
+double minSpeed = 70;
 double maxSpeed = 255;
+long currentTime = 0;
+long elapsedTime = 0;
+long prevTime = 0;
+double sampleTime = 0;
 
-double kp = 23;
-double ki = 200;
-double kd = 0;
-
-ArduPID pid;
+double kp = 18;
 
 void setup() {
   Serial.begin(9600);
@@ -49,52 +42,59 @@ void setup() {
   //set modes
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  wdt_disable();
+  wdt_enable(WDTO_15MS);
 
-  pid.begin(&angle, &motorSpeed, &setpoint, kp, ki, kd);
+
+  //  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  //reset watchdog
+  wdt_reset();
   //grab reading
   mpu.getEvent(&a, &g, &temp);
-
-  currentTime = millis();
-  elapsedTime = currentTime - prevTime;
-  prevTime    = currentTime;
 
   //find angle of tilt
   angle = atan2(a.acceleration.y, a.acceleration.z) * RAD_TO_DEG;
 
-  //Find error
-  error = angle - setpoint;
-  error = error < 0 ? -error : error;
-  errorSum += error;
-  errorSum = constrain(errorSum, 0, 300);
+  //get the time
+  currentTime = millis();
 
-  //compute output using PID controller
-  // pid.compute();
+  if (true) {
+    prevTime = currentTime;
+    //Find error
+    error = angle - setpoint;
+    error = error < 0 ? -error : error;
 
-  motorSpeed = (kp * error) ;//proportional
-  integral = ki * errorSum; //integral
-  motorSpeed = constrain(motorSpeed, minSpeed, maxSpeed);
+    //proportional
+    motorSpeed = minSpeed + (kp * error);
+    motorSpeed = constrain(motorSpeed, minSpeed, maxSpeed);
 
-  // Serial.print("Angle: ");
-  // Serial.println(angle);
-  Serial.print("Speed: ");
-  Serial.println(motorSpeed);
+    //print data
+    // Serial.print("Error: ");
+    // Serial.println(error);
+    // Serial.print("Angle: ");
+    // Serial.println(angle);
+    Serial.print("Speed: ");
+    Serial.println(motorSpeed);
 
-  motor1.setSpeed(motorSpeed);
-  motor2.setSpeed(motorSpeed);
-  if (angle > setpoint && error > frontMargin) {
-    //falling forward
-    motor1.run(BACKWARD);
-    motor2.run(BACKWARD);
-  } else if (angle < setpoint && error > backMargin) {
-    //falling backward
-    motor1.run(FORWARD);
-    motor2.run(FORWARD);
-  } else if(angle == setpoint){
-    //at setpoint
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
+    motor1.setSpeed(motorSpeed);
+    motor2.setSpeed(motorSpeed);
+    if (angle > setpoint && error > frontMargin) {
+      //falling forward
+      motor1.run(FORWARD);
+      motor2.run(FORWARD);
+    } else if (angle < setpoint && error > backMargin) {
+      //falling backward
+      motor1.run(BACKWARD);
+      motor2.run(BACKWARD);
+    } else {
+      //at setpoint
+      motor1.run(RELEASE);
+      motor2.run(RELEASE);
+    }
+  } else {
+    Serial.println("not enough time passed");
   }
 }
