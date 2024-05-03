808aedf8c032c1df622c2cdc4ec12f231d0e1328
Veniot <w0404614@campus.nscc.ca>
2024-05-03 14:20:55 -0300

    Adding gyrorate to fix stuttering

--- Self-Balancing-Robot/Self-Balancing-Robot.ino
+++ Self-Balancing-Robot/Self-Balancing-Robot.ino
@@ -13,20 +13,21 @@ AF_DCMotor motor2(4);
 sensors_event_t a, g, temp;
 
 double error = 0;
+double gyroRate = 0;
 double frontMargin = 0.75;
 double backMargin = 0.75;
 
 double setpoint = 86.35;
 double angle;
 double motorSpeed = 0;
-double minSpeed = 70;
+double minSpeed = 0;
 double maxSpeed = 255;
 long currentTime = 0;
 long elapsedTime = 0;
 long prevTime = 0;
 double sampleTime = 0;
 
-double kp = 18;
+double kp = 23;
 
 void setup() {
   Serial.begin(9600);
@@ -53,48 +54,53 @@ void loop() {
   //reset watchdog
   wdt_reset();
   //grab reading
-  mpu.getEvent(&a, &g, &temp);
+  if (mpu.getEvent(&a, &g, &temp)) {
+    //find angle of tilt
+    angle = atan2(a.acceleration.y, a.acceleration.z) * RAD_TO_DEG;
+    // gyroRate = g.gyro.y;
 
-  //find angle of tilt
-  angle = atan2(a.acceleration.y, a.acceleration.z) * RAD_TO_DEG;
+    //get the time
+    currentTime = millis();
+    elapsedTime = currentTime - prevTime;
 
-  //get the time
-  currentTime = millis();
+    if (elapsedTime > sampleTime) {
+      prevTime = currentTime;
+      //Find error
+      error = angle - setpoint;
+      // error = abs(error);
 
-  if (true) {
-    prevTime = currentTime;
-    //Find error
-    error = angle - setpoint;
-    error = error < 0 ? -error : error;
+      //proportional
+      motorSpeed = minSpeed + (kp * error);
+      motorSpeed = constrain(motorSpeed, minSpeed, maxSpeed);
 
-    //proportional
-    motorSpeed = minSpeed + (kp * error);
-    motorSpeed = constrain(motorSpeed, minSpeed, maxSpeed);
+      //print data
+      // Serial.print("Error: ");
+      // Serial.println(error);
+      // Serial.print("Speed: ");
+      // Serial.println(motorSpeed);
+      Serial.print("GyroRate Y: ");
+      Serial.println(gyroRate);
 
-    //print data
-    // Serial.print("Error: ");
-    // Serial.println(error);
-    // Serial.print("Angle: ");
-    // Serial.println(angle);
-    Serial.print("Speed: ");
-    Serial.println(motorSpeed);
-
-    motor1.setSpeed(motorSpeed);
-    motor2.setSpeed(motorSpeed);
-    if (angle > setpoint && error > frontMargin) {
-      //falling forward
-      motor1.run(FORWARD);
-      motor2.run(FORWARD);
-    } else if (angle < setpoint && error > backMargin) {
-      //falling backward
-      motor1.run(BACKWARD);
-      motor2.run(BACKWARD);
+      // motor1.setSpeed(motorSpeed);
+      // motor2.setSpeed(motorSpeed);
+      if (angle > setpoint && error > frontMargin) {
+        //falling forward
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+      } else if (angle < setpoint && error > backMargin) {
+        //falling backward
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+      } else {
+        //at setpoint
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
+      }
     } else {
-      //at setpoint
-      motor1.run(RELEASE);
-      motor2.run(RELEASE);
+      Serial.println("not enough time passed");
     }
-  } else {
-    Serial.println("not enough time passed");
+
+      error = 0;
   }
+
 }
